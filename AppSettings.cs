f5501523acce2cf947e75ee7b688d78a9afca1fc
nabuk <niemyjski.jakub@gmail.com>
2013-06-24 22:30:23 +0200

    Init

---
+++ AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace T4StrongAppSettings
+{
+	public class AppSettings
+	{
+		public string FirstName { get { return ConfigurationManager.AppSettings["FirstName"]; } }
+		public string LastName { get { return ConfigurationManager.AppSettings["LastName"]; } }
+		public string Birth { get { return ConfigurationManager.AppSettings["Birth"]; } }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace T4StrongAppSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new AppSettings();
+
+            Console.WriteLine(settings.FirstName);
+            Console.WriteLine(settings.LastName);
+            Console.WriteLine(settings.Birth);
+
+            Console.ReadLine();
+        }
+    }
+}
